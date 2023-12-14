9c1dba96b02bded717e1936fc672e7ec2d1a95de
laura <69944724+laurazp@users.noreply.github.com>
2023-12-14 01:27:17 +0100

    MainView updated with LogIn and SignUp buttons

--- Lovecook/Features/Main/MainView.swift
+++ Lovecook/Features/Main/MainView.swift
@@ -10,41 +10,58 @@ import SwiftUI
 struct MainView: View {
     var body: some View {
         
-        VStack(spacing: 26) {
-            Spacer()
-            
-            Text("LoveCook")
-                .foregroundStyle(.white)
-                .fontWeight(.bold)
-                .font(.largeTitle)
-            Spacer()
-            Spacer()
-            Button {
+        NavigationStack {
+            VStack(spacing: 18) {
+                Spacer()
                 
-            } label: {
-                Text("Log in")
-                    .frame(maxWidth: .infinity)
-            }
-            .buttonStyle(.borderedProminent)
-            .controlSize(.large)
-            Button {
+                Text("LoveCook")
+                    .foregroundStyle(.white)
+                    .fontWeight(.bold)
+                    .font(.largeTitle)
+                Spacer()
+                Spacer()
                 
-            } label: {
-                Text("Sign up")
-                    .frame(maxWidth: .infinity)
+                Button {} label: {
+                    NavigationLink(destination: LogInView()) {
+                        Text("Log in")
+                            .frame(maxWidth: .infinity)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.white)}
+                }
+                .cornerRadius(20)
+                .buttonStyle(.borderedProminent)
+                .tint(Color.lightGreen)
+                .controlSize(.large)
+                
+                Text("Don't have an account yet?")
+                    .foregroundStyle(.white)
+                    .opacity(0.8)
+                    .font(.caption2)
+                
+                NavigationLink(destination: SignUpView()) {
+                    Button("Sign up") {}
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(.white)
+                        .cornerRadius(20)
+                        .foregroundStyle(.lightGreen)
+                        .fontWeight(.bold)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke(.lightGreen, lineWidth: 1.7)
+                        )
+                }
+                Spacer()
             }
-            .buttonStyle(.bordered)
-            .controlSize(.large)
-            Spacer()
+            .padding(24)
+            .background(
+                Image("food_background")
+                    .resizable()
+                    .edgesIgnoringSafeArea(.all)
+                    .aspectRatio(contentMode: .fill)
+                    .opacity(0.8)
+            )
         }
-        .padding(24)
-        .background(
-            Image("food_background")
-                .resizable()
-                .edgesIgnoringSafeArea(.all)
-                .aspectRatio(contentMode: .fill)
-                .opacity(0.8)
-        )
     }
 }
 
