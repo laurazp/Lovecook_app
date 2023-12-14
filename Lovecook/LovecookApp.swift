9859a5142651e5cf16aaaffa9592c5123cf11eb8
laura <69944724+laurazp@users.noreply.github.com>
2023-12-14 04:11:52 +0100

    Firebase config error fixed

--- Lovecook/LovecookApp.swift
+++ Lovecook/LovecookApp.swift
@@ -14,7 +14,10 @@ import GoogleSignIn
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        FirebaseApp.configure()
+        
+        if FirebaseApp.app() == nil {
+            FirebaseApp.configure()
+            }
         
         return true
     }
