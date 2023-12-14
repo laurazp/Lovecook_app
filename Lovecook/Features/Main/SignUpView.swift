c1018977a31272a27439d86126ec81b329afa888
laura <69944724+laurazp@users.noreply.github.com>
2023-12-14 21:23:33 +0100

    SignUpView created and CheckboxToggleStyle added

--- Lovecook/Features/Main/SignUpView.swift
+++ Lovecook/Features/Main/SignUpView.swift
@@ -8,8 +8,49 @@
 import SwiftUI
 
 struct SignUpView: View {
+    @State private var newUserName: String = ""
+    @State private var newUserEmail: String = ""
+    @State private var newUserPassword: String = ""
+    @State private var isAccepted: Bool = false
+    
     var body: some View {
-        Text("SignUp")
+        VStack(spacing: 18) {
+            Text("SIGN UP")
+            Text("Thanks for joining us!")
+                
+            TextField("Enter your name", text: $newUserName)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            TextField("Enter your email", text: $newUserEmail)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            SecureField("Choose a password", text: $newUserPassword)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            //TODO: Checkbox
+            Toggle(isOn: $isAccepted) {
+                Text("I have read and agree to the Terms and Conditions")
+                    .foregroundStyle(.gray)
+                    .font(.footnote)
+            }
+            .toggleStyle(CheckboxToggleStyle.init())
+            
+            Button {
+                //TODO: check and save user data
+            } label: {
+                //TODO: login o home? ckechear primero
+                NavigationLink(destination: HomeView()) {
+                    Text("Sign up")
+                        .frame(maxWidth: .infinity)
+                        .fontWeight(.bold)
+                    .foregroundColor(Color.white)}
+            }
+            .cornerRadius(20)
+            .buttonStyle(.borderedProminent)
+            .tint(Color.lightGreen)
+            .controlSize(.large)
+        }
+        .padding()
     }
 }
 
