6e51be452d53f2802cc248ef90002c1440f93896
laurazp <laurikazp@gmail.com>
2023-12-27 21:11:45 +0100

    MealsByCategory updated not showing link arrow. App logo added.

--- Lovecook/Features/MealsByCategory/MealsByCategoryView/MealItemView.swift
+++ Lovecook/Features/MealsByCategory/MealsByCategoryView/MealItemView.swift
@@ -19,8 +19,7 @@ struct MealItemView: View {
                     .aspectRatio(contentMode: .fill)
                     .clipShape(RoundedRectangle(cornerRadius: 12))
                     .shadow(color: .black, radius: 7, x: 3, y: 5)
-                    //.padding(12)
-                    .frame(width: 320, height: 170)
+                    .frame(width: 350, height: 170)
                     .background(.black)
                     .cornerRadius(16)
             } else {
@@ -29,15 +28,14 @@ struct MealItemView: View {
                     .aspectRatio(contentMode: .fill)
                     .clipShape(RoundedRectangle(cornerRadius: 12))
                     .shadow(color: .black, radius: 7, x: 3, y: 5)
-                    //.padding(12)
-                    .frame(width: 320, height: 170)
+                    .frame(width: 350, height: 170)
                     .background(.black)
                     .cornerRadius(16)
             }
             
             
                 Text(meal.mealTitle)
-                    .padding(.leading, 18)
+                    .padding()
                     .font(.title)
                     .foregroundStyle(.white)
                     .bold()
