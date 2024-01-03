e86f601bd50111bb31592e25856d16404c3676be
laurazp <laurikazp@gmail.com>
2024-01-03 23:22:01 +0100

    Recipe models and mapper updated to show and map ingredients & measures

--- Lovecook/Entities/APIResponses/Recipes/ApiRecipe.swift
+++ Lovecook/Entities/APIResponses/Recipes/ApiRecipe.swift
@@ -5,7 +5,6 @@
 //  Created by Laura Zafra Prat on 22/11/23.
 //
 
-
 import Foundation
 
 struct ApiRecipe: Codable, Identifiable {
@@ -13,7 +12,6 @@ struct ApiRecipe: Codable, Identifiable {
         recipeId
     }
     
-    //TODO: borrar parámetros que no vaya a usar
     let recipeId: String
     let recipeTitle: String?
     let recipeCategory: String
@@ -22,49 +20,47 @@ struct ApiRecipe: Codable, Identifiable {
     let recipeImage: String?
     let recipeTags: String?
     let recipeYoutubeUrl: String?
-    /*let strIngredient1: String?
-    let strIngredient2: String?
-    let strIngredient3: String?
-    let strIngredient4: String?
-    let strIngredient5: String?
-    let strIngredient6: String?
-    let strIngredient7: String?
-    let strIngredient8: String?
-    let strIngredient9: String?
-    let strIngredient10: String?
-    let strIngredient11: String?
-    let strIngredient12: String?
-    let strIngredient13: String?
-    let strIngredient14: String?
-    let strIngredient15: String?
-    let strIngredient16: String?
-    let strIngredient17: String?
-    let strIngredient18: String?
-    let strIngredient19: String?
-    let strIngredient20: String?
-    let strMeasure1: String?
-    let strMeasure2: String?
-    let strMeasure3: String?
-    let strMeasure4: String?
-    let strMeasure5: String?
-    let strMeasure6: String?
-    let strMeasure7: String?
-    let strMeasure8: String?
-    let strMeasure9: String?
-    let strMeasure10: String?
-    let strMeasure11: String?
-    let strMeasure12: String?
-    let strMeasure13: String?
-    let strMeasure14: String?
-    let strMeasure15: String?
-    let strMeasure16: String?
-    let strMeasure17: String?
-    let strMeasure18: String?
-    let strMeasure19: String?
-    let strMeasure20: String?*/
-    
-    //TODO: init from decoder
-    
+    let ingredient1: String?
+    let ingredient2: String?
+    let ingredient3: String?
+    let ingredient4: String?
+    let ingredient5: String?
+    let ingredient6: String?
+    let ingredient7: String?
+    let ingredient8: String?
+    let ingredient9: String?
+    let ingredient10: String?
+    let ingredient11: String?
+    let ingredient12: String?
+    let ingredient13: String?
+    let ingredient14: String?
+    let ingredient15: String?
+    let ingredient16: String?
+    let ingredient17: String?
+    let ingredient18: String?
+    let ingredient19: String?
+    let ingredient20: String?
+    let measure1: String?
+    let measure2: String?
+    let measure3: String?
+    let measure4: String?
+    let measure5: String?
+    let measure6: String?
+    let measure7: String?
+    let measure8: String?
+    let measure9: String?
+    let measure10: String?
+    let measure11: String?
+    let measure12: String?
+    let measure13: String?
+    let measure14: String?
+    let measure15: String?
+    let measure16: String?
+    let measure17: String?
+    let measure18: String?
+    let measure19: String?
+    let measure20: String?
+        
     enum CodingKeys: String, CodingKey {
         case recipeId = "idMeal"
         case recipeTitle = "strMeal"
@@ -74,5 +70,45 @@ struct ApiRecipe: Codable, Identifiable {
         case recipeImage = "strMealThumb"
         case recipeTags = "strTags"
         case recipeYoutubeUrl = "strYoutube"
+        case ingredient1 = "strIngredient1"
+        case ingredient2 = "strIngredient2"
+        case ingredient3 = "strIngredient3"
+        case ingredient4 = "strIngredient4"
+        case ingredient5 = "strIngredient5"
+        case ingredient6 = "strIngredient6"
+        case ingredient7 = "strIngredient7"
+        case ingredient8 = "strIngredient8"
+        case ingredient9 = "strIngredient9"
+        case ingredient10 = "strIngredient10"
+        case ingredient11 = "strIngredient11"
+        case ingredient12 = "strIngredient12"
+        case ingredient13 = "strIngredient13"
+        case ingredient14 = "strIngredient14"
+        case ingredient15 = "strIngredient15"
+        case ingredient16 = "strIngredient16"
+        case ingredient17 = "strIngredient17"
+        case ingredient18 = "strIngredient18"
+        case ingredient19 = "strIngredient19"
+        case ingredient20 = "strIngredient20"
+        case measure1 = "strMeasure1"
+        case measure2 = "strMeasure2"
+        case measure3 = "strMeasure3"
+        case measure4 = "strMeasure4"
+        case measure5 = "strMeasure5"
+        case measure6 = "strMeasure6"
+        case measure7 = "strMeasure7"
+        case measure8 = "strMeasure8"
+        case measure9 = "strMeasure9"
+        case measure10 = "strMeasure10"
+        case measure11 = "strMeasure11"
+        case measure12 = "strMeasure12"
+        case measure13 = "strMeasure13"
+        case measure14 = "strMeasure14"
+        case measure15 = "strMeasure15"
+        case measure16 = "strMeasure16"
+        case measure17 = "strMeasure17"
+        case measure18 = "strMeasure18"
+        case measure19 = "strMeasure19"
+        case measure20 = "strMeasure20"
     }
 }
--- Lovecook/Entities/Models/Recipe.swift
+++ Lovecook/Entities/Models/Recipe.swift
@@ -19,75 +19,8 @@ struct Recipe {
     let recipeImage: String
     let recipeTags: [String]
     let recipeYoutubeUrl: String
-    /*let strIngredient1: String?
-    let strIngredient2: String?
-    let strIngredient3: String?
-    let strIngredient4: String?
-    let strIngredient5: String?
-    let strIngredient6: String?
-    let strIngredient7: String?
-    let strIngredient8: String?
-    let strIngredient9: String?
-    let strIngredient10: String?
-    let strIngredient11: String?
-    let strIngredient12: String?
-    let strIngredient13: String?
-    let strIngredient14: String?
-    let strIngredient15: String?
-    let strIngredient16: String?
-    let strIngredient17: String?
-    let strIngredient18: String?
-    let strIngredient19: String?
-    let strIngredient20: String?
-    let strMeasure1: String?
-    let strMeasure2: String?
-    let strMeasure3: String?
-    let strMeasure4: String?
-    let strMeasure5: String?
-    let strMeasure6: String?
-    let strMeasure7: String?
-    let strMeasure8: String?
-    let strMeasure9: String?
-    let strMeasure10: String?
-    let strMeasure11: String?
-    let strMeasure12: String?
-    let strMeasure13: String?
-    let strMeasure14: String?
-    let strMeasure15: String?
-    let strMeasure16: String?
-    let strMeasure17: String?
-    let strMeasure18: String?
-    let strMeasure19: String?
-    let strMeasure20: String?*/
-    
-    /*init(recipeId: String, recipeTitle: String, recipeCategory: String, recipeArea: String, recipeInstructions: String, recipeImage: String, recipeTags: [String], recipeYoutubeUrl: String, ingredients: [String?], measures: [String?]) {
-        self.recipeId = recipeId
-        self.recipeTitle = recipeTitle
-        self.recipeCategory = recipeCategory
-        self.recipeArea = recipeArea
-        self.recipeInstructions = recipeInstructions
-        self.recipeImage = recipeImage
-        self.recipeTags = recipeTags
-        self.recipeYoutubeUrl = recipeYoutubeUrl
-        
-        // Ensure that the number of ingredients and measures doesn't exceed 20
-        let ingredientCount = min(ingredients.count, 20)
-        let measureCount = min(measures.count, 20)
-        
-        // Initialize optional ingredient properties
-        self.strIngredient1 = ingredientCount > 0 ? ingredients[0] : nil
-        self.strIngredient2 = ingredientCount > 1 ? ingredients[1] : nil
-        self.strIngredient3 = ingredientCount > 2 ? ingredients[2] : nil
-        self.strIngredient4 = ingredientCount > 3 ? ingredients[3] : nil
-        self.strIngredient5 = ingredientCount > 4 ? ingredients[4] : nil
-        // ... continue for strIngredient3 to strIngredient20
-        
-        // Initialize optional measure properties
-        self.strMeasure1 = measureCount > 0 ? measures[0] : nil
-        self.strMeasure2 = measureCount > 1 ? measures[1] : nil
-        // ... continue for strMeasure3 to strMeasure20
-    }*/
-    
+    var ingredients: [Ingredient]
+
     static var example: Recipe {
         Recipe(
             recipeId: "5346",
@@ -97,9 +30,8 @@ struct Recipe {
             recipeInstructions: "Istructions here blabla bla blabla bla blabla bla blabla bla blabla bla blabla bla blabla bla blabla bla blabla bla blabla bla",
             recipeImage: "https://www.themealdb.com/images/media/meals/wrssvt1511556563.jpg",
             recipeTags: ["Drink", "Fit", "Cool"],
-            recipeYoutubeUrl: "Youtube url here"/*,
-            ingredients: ["Oysters", "Butter", "Chicken soup"],
-            measures: ["1 cup", "2 tbsp", "2 cups"]*/
+            recipeYoutubeUrl: "Youtube url here",
+            ingredients: [Ingredient(ingredient: "Oysters", measure: "1kg"), Ingredient(ingredient: "Butter", measure: "2tbs"), Ingredient(ingredient: "Chicken soup", measure: "1L")]
         )
     }
 }
