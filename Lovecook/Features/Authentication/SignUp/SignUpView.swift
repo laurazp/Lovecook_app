83a724fe29a36de01f84b80ea4d336468f62be8d
laurazp <laurikazp@gmail.com>
2024-01-07 17:57:02 +0100

    Cleanup and layout updates

--- Lovecook/Features/Authentication/SignUp/SignUpView.swift
+++ Lovecook/Features/Authentication/SignUp/SignUpView.swift
@@ -8,33 +8,46 @@
 import SwiftUI
 
 struct SignUpView: View {
+    
+    private struct Layout {
+        static let signUpTitle = "SIGN UP"
+        static let signUpSubtitle = "Thanks for joining us!"
+        static let emailSubtitle = "- or continue with e-mail -"
+        static let emailHint = "Enter your Email"
+        static let passwordHint = "Choose a password"
+        static let signUpButtonTitle = "Sign up"
+        static let termsAndConditionsText = "I have read and agree to the Terms and Conditions"
+        static let subtitleSize: CGFloat = 16
+        static let titleSize: CGFloat = 20
+    }
+    
     @EnvironmentObject var viewModel: AuthenticationViewModel
     
     var body: some View {
         VStack(spacing: 24) {
-            Text("SIGN UP")
-                .font(Font.system(size: 20))
+            Text(Layout.signUpTitle)
+                .font(Font.system(size: Layout.titleSize))
                 .fontWeight(Font.Weight.bold)
                 .foregroundColor(Color.darkBlue)
-            Text("Thanks for joining us!")
-                .font(Font.system(size: 16))
+            Text(Layout.signUpSubtitle)
+                .font(Font.system(size: Layout.subtitleSize))
                 .fontWeight(Font.Weight.bold)
                 .foregroundColor(Color.gray)
             
-            TextField("Enter your email", text: $viewModel.userEmail)
+            TextField(Layout.emailHint, text: $viewModel.userEmail)
                 .authenticationTextfieldStyle()
             
-            SecureField("Choose a password", text: $viewModel.userPassword)
+            SecureField(Layout.passwordHint, text: $viewModel.userPassword)
                 .authenticationTextfieldStyle()
             
             Toggle(isOn: $viewModel.isAccepted) {
-                Text("I have read and agree to the Terms and Conditions")
+                Text(Layout.termsAndConditionsText)
                     .foregroundStyle(.gray)
                     .font(.footnote)
             }
             .toggleStyle(CheckboxToggleStyle.init())
             
-            GreenRoundedButton(buttonText: "Sign up") {
+            GreenRoundedButton(buttonText: Layout.signUpButtonTitle) {
                 viewModel.register()
             }
         }
